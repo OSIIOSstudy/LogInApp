cd1e4f34338a073f81c9f93e37a16730e0ca0f67
OSIIOSdev <OSIIOS.dev@gmail.com>
2024-03-14 00:21:58 -0700

    1. UI elements added and positioned 2. Segue configured 3. Button action methods configured 4. Code optimized 5. Marks added

--- LogInApp/LogInViewController.swift
+++ LogInApp/LogInViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class LogInViewController: UIViewController {
+    // MARK: - @IBOutlets
+    
+    
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
